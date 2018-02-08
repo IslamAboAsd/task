6964502c01116081e3bcd9fbdb8d7269aefcab05
eslam200788 <eslam200788@gmail.com>
2018-02-08 18:54:41 +0200

    تتت

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@ var myApp = angular.module("myApp", ["ngRoute", "personController"]);
 myApp.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "partail/main.htm",
+        templateUrl : "../partail/main.htm",
     })
     .when("/london", {
-        templateUrl : "partail/london.htm",
+        templateUrl : "../partail/london.htm",
         controller : "londonCtrl"
     })
     .when("/paris", {
-        templateUrl : "partail/paris.htm",
+        templateUrl : "../partail/paris.htm",
         controller : "parisCtrl"
     })
     .otherwise({
