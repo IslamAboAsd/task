8a284be98e9108c4e14ee57f0a7e51edd52c60af
eslam200788 <eslam200788@gmail.com>
2018-02-08 16:54:47 +0200

    dd

---
+++ js/controller.js
@@ -0,0 +1,9 @@
+var personController=angular.module('personController',[]);
+personController.controller('ListController',function($scope){
+   $scope.name='sameh algndi';
+   
+});
+personController.controller('DetailsController',function($scope){
+   $scope.Details='he is agood programmer';
+   
+});
