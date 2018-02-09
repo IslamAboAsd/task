6d01a4d21e4d6420e6eda73569dee96c2ac029aa
eslam200788 <eslam200788@gmail.com>
2018-02-10 00:47:44 +0200

    hidden

--- js/main.js
+++ js/main.js
@@ -2,12 +2,7 @@ jQuery(document).ready(function(){
 
     "use strict";
 
-$("").click( function () {
-    $(this).toggleClass('active');
-    $(this).siblings().removeClass("active");
-    
-    return false;
-});
+
 $(".close").on("click", function(){
     $(this).parent().parent().addClass('hidden')
 })
