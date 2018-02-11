74e400e91caa719a17fc38a0103cb3d3d62a01d8
eslam200788 <eslam200788@gmail.com>
2018-02-11 22:27:17 +0200

    gg

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ jQuery(document).ready(function(){
 
 
     $(".close").on("click", function(){ $(this).parent().parent().addClass('hidden')});
-
     $(".big").on("click", function(){ $(this).parent().parent().parent().toggleClass('bigger').siblings().toggleClass('hidden') });
+   
+    $('.cus').click(function () {
+
+        $('aside').toggleClass('hidden-aside');
+        $('.veiw').toggleClass('add-width');
+        $('.myNav').toggleClass('add-width-nav');
+
+       
+    })
 });
